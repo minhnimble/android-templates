fcfdea5565005bf6c1ef52d8b259544b07f5f556
hoangnguyen92dn <hoang.l@nimblehq.co>
2020-06-25 15:56:32 +0700

    Update base components for activity and fragment

--- app/src/main/java/co/nimblehq/ui/base/BaseActivity.kt
+++ app/src/main/java/co/nimblehq/ui/base/BaseActivity.kt
@@ -1,38 +1,62 @@
 package co.nimblehq.ui.base
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import dagger.android.AndroidInjection
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
+import androidx.annotation.LayoutRes
+import androidx.lifecycle.ViewModelProvider
+import co.nimblehq.di.ActivityViewModelFactory
+import co.nimblehq.extension.userReadableMessage
+import co.nimblehq.ui.common.Toaster
+import dagger.android.support.DaggerAppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.addTo
 import javax.inject.Inject
+import kotlin.reflect.KClass
 
-abstract class BaseActivity: AppCompatActivity(), HasSupportFragmentInjector {
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+abstract class BaseActivity<VM : BaseViewModel> : DaggerAppCompatActivity() {
 
-    private var disposables = CompositeDisposable()
+    @Inject lateinit var viewModelFactory: ActivityViewModelFactory
+    @Inject lateinit var toaster: Toaster
 
+    protected val viewModel: VM by lazy { viewModel() }
+
+    protected abstract val viewModelClass: KClass<VM>
+
+    @get:LayoutRes
+    protected abstract val layoutRes: Int
+
+    private val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-    }
+        setContentView(layoutRes)
 
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
-        return this.dispatchingAndroidInjector
-    }
-
-    protected fun Disposable.bindForDisposable() {
-        disposables.add(this)
+        bindViewModel()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        disposables.clear()
+        disposables.dispose()
     }
+
+    abstract fun bindViewModel()
+
+    protected fun Disposable.addToDisposables() = addTo(disposables)
+
+    private fun viewModel(): VM =
+        ViewModelProvider(this, viewModelFactory)[viewModelClass.java]
+
+    protected inline infix fun <T> Observable<T>.bindTo(crossinline action: (T) -> Unit) {
+        observeOn(AndroidSchedulers.mainThread())
+            .subscribe { action(it) }
+            .addToDisposables()
+    }
+
+    fun displayError(error: Throwable) {
+        val message = error.userReadableMessage(this)
+        toaster.display(message)
+    }
+
 }
